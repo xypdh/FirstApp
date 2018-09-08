b2e8fe8e8d857b9cc9d216621d7f80e32c70c5bd
xypdh <149462963@qq.com>
2018-09-08 21:24:58 +0800

    增加

--- First/src/First.java
+++ First/src/First.java
@@ -6,4 +6,9 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
     }
+    public void  Add(){
+       int a=1,b=20;
+       int c=a+b;
+        System.out.println("c");
+    }
 }
