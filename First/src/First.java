5825dbbaea1020d3c5a770926bb40c4d4353ab03
xypdh <149462963@qq.com>
2018-09-08 22:17:33 +0800

    增加

--- First/src/First.java
+++ First/src/First.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @author xy
  * @create 2018-09-08-21:17
@@ -5,10 +7,16 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        Add();
     }
-    public void  Add(){
+    public static void  Add(){
        int a=1,b=20;
        int c=a+b;
         System.out.println("c");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","tom");
+        map.put("age","10");
+        String ss = map.get("name");
+        System.out.println("ss = " + ss);
     }
 }
